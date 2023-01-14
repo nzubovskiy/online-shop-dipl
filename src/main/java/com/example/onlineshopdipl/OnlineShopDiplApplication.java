3fd54767eb9531425450edaaac549b9a9c336d03
NIKO <nzubovskiy@gmail.com>
2023-01-14 16:59:41 +0400

    first commit

---
+++ src/main/java/com/example/onlineshopdipl/OnlineShopDiplApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlineshopdipl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopDiplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineShopDiplApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/onlineshopdipl/OnlineShopDiplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlineshopdipl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopDiplApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
