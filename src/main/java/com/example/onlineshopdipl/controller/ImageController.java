6052ee4e365dc47e78057bdf8ba8c1274f4a9a33
NIKO <nzubovskiy@gmail.com>
2023-03-11 13:05:40 +0400

    Ads Controller, ImageController, UserController fix

--- src/main/java/com/example/onlineshopdipl/controller/ImageController.java
+++ src/main/java/com/example/onlineshopdipl/controller/ImageController.java
@@ -2,16 +2,13 @@ package com.example.onlineshopdipl.controller;
 
 import com.example.onlineshopdipl.service.ImageService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+
 
 
 @Slf4j
@@ -21,35 +18,21 @@ public class ImageController {
 
     private final ImageService imageService;
     public ImageController(ImageService imageService){
-        this.imageService=imageService;
+        this.imageService = imageService;
     }
 
-    @Operation(
-            tags = "Изображения",
-            summary = "updateAdsImage",
-            operationId = "updateImage",
+    @Operation(summary = "getAdsImage",
             responses = {
-                    @ApiResponse(responseCode = "200", description = "OK", content = {
-                            @Content(mediaType = "application/octet-stream", schema = @Schema(implementation = byte[].class))
-                    }),
-                    @ApiResponse(responseCode = "404", description = "Not Found")
-            }
-    )
-    @PreAuthorize("hasRole('USER')or hasRole('ADMIN')")
-    @RequestMapping(
-            method = RequestMethod.PATCH,
-            value = "/image/{id}",
-            produces = { "application/octet-stream" },
-            consumes = { "multipart/form-data" }
-    )
-    public ResponseEntity<Void> updateAdsImage(
-            @Parameter(name = "id", required = true) @PathVariable("id") Integer id,
-            @Parameter(name = "image", required = true) @RequestPart(value = "image") MultipartFile image,
-            Authentication authentication)
-    {
-        imageService.updateAdsImage(id, image, authentication);
-        return ResponseEntity.ok().build();
+                    @ApiResponse(responseCode = "200"),
+                    @ApiResponse(responseCode = "404", content = @Content)
+            })
+    @GetMapping(value = "{id}", produces = {MediaType.IMAGE_PNG_VALUE})
+    public ResponseEntity<byte[]> getAdsImage(@PathVariable Integer id) {
+        return ResponseEntity.ok(imageService.getImage(id));
     }
+
+
+
 
 
 }
